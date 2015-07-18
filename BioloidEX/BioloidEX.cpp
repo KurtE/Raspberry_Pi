3edcff96a7112434715d0371b309190b66be566f
Kurt Eckhardt <kurte@rockisland.com>
2015-07-17 17:20:53 -0700

    WIP - BioloidEX use worker thread...
    Again warning - WIP - Also github having issues in this directory.

--- BioloidEX/BioloidEX.cpp
+++ BioloidEX/BioloidEX.cpp
@@ -21,6 +21,7 @@
 #include "dynamixel.h"
 #include "ax12.h"
 #include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
 #define delay(x)  usleep((x)*1000)
 #define delayMicroseconds(us) usleep((us))
@@ -30,48 +31,57 @@ extern unsigned long micros(void);
 
 /* initializes serial1 transmit at baud, 8-N-1 */
 BioloidControllerEx::BioloidControllerEx(long baud){
-  int i;
-  // setup storage
-  id_ = (unsigned char *) malloc(AX12_MAX_SERVOS * sizeof(unsigned char));
-  pose_ = (unsigned int *) malloc(AX12_MAX_SERVOS * sizeof(unsigned int));
-  nextpose_ = (unsigned int *) malloc(AX12_MAX_SERVOS * sizeof(unsigned int));
-  speed_ = (int *) malloc(AX12_MAX_SERVOS * sizeof(int));
-  // initialize
-  for(i=0;i<AX12_MAX_SERVOS;i++){
-    id_[i] = i+1;
-    pose_[i] = 512;
-    nextpose_[i] = 512;
-  }
-  frameLength = BIOLOID_FRAME_LENGTH;
-  interpolating_ = 0;
-  playing = 0;
-  nextframe_ = millis();
-  
-  //ax12Init(baud);  
-  // dxl Initialize takes baud number, not rate...
-  if(dxl_initialize(0, 1) == 0) {
-	//printf("Failed to open USBDynamixel\n");
-  }	
+    int i;
+    // setup storage
+    id_ = (unsigned char *) malloc(AX12_MAX_SERVOS * sizeof(unsigned char));
+    pose_ = (unsigned int *) malloc(AX12_MAX_SERVOS * sizeof(unsigned int));
+    nextpose_ = (unsigned int *) malloc(AX12_MAX_SERVOS * sizeof(unsigned int));
+    // initialize
+    for(i=0;i<AX12_MAX_SERVOS;i++){
+        id_[i] = i+1;
+        pose_[i] = 512;
+        nextpose_[i] = 512;
+    }
+    frameLength = BIOLOID_FRAME_LENGTH;
+    interpolating_ = 0;
+    playing = 0;
+    signal_new_pose_ = 0;
+    
+    // dxl Initialize takes baud number, not rate...
+    if(dxl_initialize(0, 1) == 0) {
+        //printf("Failed to open USBDynamixel\n");
+    }	
+
+    // Lets now create our worker thread. 
+     _fCancel = false;
+    pthread_mutex_init(&mutex_, NULL);
+    pthread_cond_init(&cond_, NULL);
+	pthread_barrier_init(&_barrier, 0, 2);
+    pthread_create(&tidWorker, NULL, &WorkerThreadProc, this);
+    
+  	// sync startup
+	pthread_barrier_wait(&_barrier);
+
+
+
 }
 
 /* new-style setup */
 void BioloidControllerEx::setup(int servo_cnt){
-  int i;
-  // setup storage
-  id_ = (unsigned char *) malloc(servo_cnt * sizeof(unsigned char));
-  pose_ = (unsigned int *) malloc(servo_cnt * sizeof(unsigned int));
-  nextpose_ = (unsigned int *) malloc(servo_cnt * sizeof(unsigned int));
-  speed_ = (int *) malloc(servo_cnt * sizeof(int));
-  // initialize
-  poseSize = servo_cnt;
-  for(i=0;i<poseSize;i++){
-    id_[i] = i+1;
-    pose_[i] = 512;
-    nextpose_[i] = 512;
-  }
-  interpolating_ = 0;
-  playing = 0;
-  nextframe_ = millis();
+    int i;
+    // setup storage
+    id_ = (unsigned char *) malloc(servo_cnt * sizeof(unsigned char));
+    pose_ = (unsigned int *) malloc(servo_cnt * sizeof(unsigned int));
+    nextpose_ = (unsigned int *) malloc(servo_cnt * sizeof(unsigned int));
+    // initialize
+    posesize_ = servo_cnt;
+    for(i=0;i<posesize_;i++){
+        id_[i] = i+1;
+        pose_[i] = 512;
+        nextpose_[i] = 512;
+    }
+    interpolating_ = 0;
+    playing = 0;
 }
 void BioloidControllerEx::setId(int index, int id){
   id_[index] = id;
@@ -82,19 +92,30 @@ int BioloidControllerEx::getId(int index){
 
 /* load a named pose from FLASH into nextpose. */
 void BioloidControllerEx::loadPose( const unsigned int * addr ){
-  int i;
-  poseSize = (int)*addr; // number of servos in this pose
-  for(i=0; i<poseSize; i++)
-    nextpose_[i] = *(addr+1+i) << BIOLOID_SHIFT;
+    int i;
+    posesize_ = (int)*addr; // number of servos in this pose
+    for(i=0; i<posesize_; i++)
+        nextpose_[i] = *(addr+1+i) << BIOLOID_SHIFT;
 }
+
+int BioloidControllerEx::poseSize(void) {
+    return posesize_;
+}                          // how many servos are in this pose, used by Sync()
+
+void BioloidControllerEx::poseSize(uint8_t posesize) {
+    posesize_ = posesize;
+}
+
+
 /* read in current servo positions to the pose. */
 void BioloidControllerEx::readPose(){
-  for(int i=0;i<poseSize;i++){
-    pose_[i] = dxl_read_word(id_[i],AX_PRESENT_POSITION_L)<<BIOLOID_SHIFT;
-    delay(25);   
-  }
+    for(int i=0;i<posesize_;i++){
+        pose_[i] = dxl_read_word(id_[i],AX_PRESENT_POSITION_L)<<BIOLOID_SHIFT;
+        delay(25);   
+    }
 }
 /* write pose out to servos using sync write. */
+#if 0
 void BioloidControllerEx::writePose(){
     int temp;
 
@@ -104,18 +125,18 @@ void BioloidControllerEx::writePose(){
     dxl_set_txpacket_parameter(0, AX_GOAL_POSITION_L);
     dxl_set_txpacket_parameter(1, 2);
 
-    for(int i=0; i<poseSize; i++ ) {
+    for(int i=0; i<posesize_; i++ ) {
         temp = pose_[i] >> BIOLOID_SHIFT;
         dxl_set_txpacket_parameter(2+3*i, id_[i]);
         dxl_set_txpacket_parameter(2+3*i+1, dxl_get_lowbyte(temp));
         dxl_set_txpacket_parameter(2+3*i+2, dxl_get_highbyte(temp));
     }
 
-    dxl_set_txpacket_length((2+1)*poseSize+4);
+    dxl_set_txpacket_length((2+1)*posesize_+4);
     dxl_txrx_packet();
     dxl_get_result();   // don't care for now
 }
-
+#endif
 /* set up for an interpolation from pose to nextpose over TIME 
  milliseconds by setting servo speeds. */
 uint8_t  BioloidControllerEx::interpolating(void) {
@@ -124,132 +145,257 @@ uint8_t  BioloidControllerEx::interpolating(void) {
 
     
 void BioloidControllerEx::interpolateSetup(int time){
-  int i;
-  int frames = (time/frameLength) + 1;
-  nextframe_ = millis() + frameLength;
-  // set speed each servo...
-  for(i=0;i<poseSize;i++){
-    if(nextpose_[i] > pose_[i]){
-      speed_[i] = (nextpose_[i] - pose_[i])/frames + 1;
-    }
-    else{
-      speed_[i] = (pose_[i]-nextpose_[i])/frames + 1;
-    }
-  }
-  interpolating_ = 1;
+
+    // Lets signal our worker thread it has work to do...
+    pthread_mutex_lock(&mutex_);
+    signal_new_pose_ = 1;
+    pose_time_ = time;
+    pthread_cond_signal(&cond_);
+    pthread_mutex_unlock(&mutex_);
+
 }
+
+
 /* interpolate our pose, this should be called at about 30Hz. */
 #define WAIT_SLOP_FACTOR 10  
 void BioloidControllerEx::interpolateStep(uint8_t fWait){
-  if(interpolating_ == 0) return;
-  int i;
-  int complete = poseSize;
-  if (!fWait) {
-    if (millis() < (nextframe_ - WAIT_SLOP_FACTOR)) {
-      return;    // We still have some time to do something... 
-    }
-  }
-  while(millis() < nextframe_) ;
-  nextframe_ = millis() + frameLength;
-  // update each servo
-  for(i=0;i<poseSize;i++){
-    int diff = nextpose_[i] - pose_[i];
-    if(diff == 0){
-      complete--;
-    }
-    else{
-      if(diff > 0){
-        if(diff < speed_[i]){
-          pose_[i] = nextpose_[i];
-          complete--;
-        }
-        else
-          pose_[i] += speed_[i];
-      }
-      else{
-        if((-diff) < speed_[i]){
-          pose_[i] = nextpose_[i];
-          complete--;
-        }
-        else
-          pose_[i] -= speed_[i];                
-      }       
-    }
-  }
-  if(complete <= 0) interpolating_ = 0;
-  writePose();      
 }
 
 /* get a servo value in the current pose */
 int BioloidControllerEx::getCurPose(int id){
-  for(int i=0; i<poseSize; i++){
-    if( id_[i] == id )
-      return ((pose_[i]) >> BIOLOID_SHIFT);
-  }
-  return -1;
+    for(int i=0; i<posesize_; i++){
+        if( id_[i] == id )
+            return ((pose_[i]) >> BIOLOID_SHIFT);
+    }
+    return -1;
 }
 /* get a servo value in the next pose */
 int BioloidControllerEx::getNextPose(int id){
-  for(int i=0; i<poseSize; i++){
-    if( id_[i] == id )
-      return ((nextpose_[i]) >> BIOLOID_SHIFT);
-  }
-  return -1;
+    for(int i=0; i<posesize_; i++){
+        if( id_[i] == id )
+            return ((nextpose_[i]) >> BIOLOID_SHIFT);
+    }
+    return -1;
 }
 
 int BioloidControllerEx::getNextPoseByIndex(int index){
-  if (index < poseSize) {
-    return (nextpose_[index] >> BIOLOID_SHIFT);
-  }
-  return -1;
+    if (index < posesize_) {
+        return (nextpose_[index] >> BIOLOID_SHIFT);
+    }
+    return -1;
 }
 
 
 /* set a servo value in the next pose */
 void BioloidControllerEx::setNextPose(int id, int pos){
-  for(int i=0; i<poseSize; i++){
-    if( id_[i] == id ){
-      nextpose_[i] = (pos << BIOLOID_SHIFT);
-      return;
+    for(int i=0; i<posesize_; i++){
+        if( id_[i] == id ){
+            nextpose_[i] = (pos << BIOLOID_SHIFT);
+            return;
+        }
     }
-  }
 }
 
 /* Added by Kurt */
 void BioloidControllerEx::setNextPoseByIndex(int index, int pos) {  // set a servo value by index for next pose
-  if (index < poseSize) {
-    nextpose_[index] = (pos << BIOLOID_SHIFT);
-  }
+    if (index < posesize_) {
+        nextpose_[index] = (pos << BIOLOID_SHIFT);
+    }
 }
+
 /* play a sequence. */
 void BioloidControllerEx::playSeq( const transition_t  * addr ){
-  sequence = (transition_t *) addr;
-  // number of transitions left to load
-  transitions = *(&sequence->time);
-  sequence++;    
-  // load a transition
-  loadPose((const unsigned int *)*(&sequence->pose));
-  interpolateSetup(*(&sequence->time));
-  transitions--;
-  playing = 1;
+    sequence = (transition_t *) addr;
+    // number of transitions left to load
+    transitions = *(&sequence->time);
+    sequence++;    
+    // load a transition
+    loadPose((const unsigned int *)*(&sequence->pose));
+    interpolateSetup(*(&sequence->time));
+    transitions--;
+    playing = 1;
 }
+
 /* keep playing our sequence */
 void BioloidControllerEx::play(){
-  if(playing == 0) return;
-  if(interpolating_ > 0){
-    interpolateStep();
-  }
-  else{  // move onto next pose
-    sequence++;   
-    if(transitions > 0){
-      loadPose((const unsigned int *)(&sequence->pose));
-      interpolateSetup(*(&sequence->time));
-      transitions--;
+    if(playing == 0) return;
+    if(interpolating_ > 0){
+        interpolateStep();
     }
-    else{
-      playing = 0;
+    else{  // move onto next pose
+        sequence++;   
+        if(transitions > 0){
+            loadPose((const unsigned int *)(&sequence->pose));
+            interpolateSetup(*(&sequence->time));
+            transitions--;
+        }
+        else{
+            playing = 0;
+        }
     }
-  }
+}
+
+
+int ax12GetRegister(int id, int regstart, int length) {
+    int iRet;
+    if (length == 1) 
+        iRet = dxl_read_byte(id, regstart);
+    else    
+        iRet = dxl_read_word(id, regstart);    
+    return ((dxl_get_result() == COMM_RXSUCCESS)? iRet : -1);
+}
+
+void ax12SetRegister(int id, int regstart, int data) {
+    dxl_write_byte(id, regstart, data & 0xff);
+}
+
+void ax12SetRegister2(int id, int regstart, int data) {
+    dxl_write_word(id, regstart, data);
+}
+
+//==============================================================================
+// Main worker thread to do interpolation and maybe some other stuff as well. 
+//==============================================================================
+void *BioloidControllerEx::WorkerThreadProc(void *pv)
+{
+    BioloidControllerEx *pbioloid = (BioloidControllerEx*)pv;
+    pthread_barrier_wait(&pbioloid->_barrier);
+
+    printf("Bioloid Thread start\n");
+
+    int i;
+    
+    // fields moved out of class... 
+    unsigned int * target_pose = NULL;                // This is what is used in the secondary thread as to not conflict...
+    int * servo_speed = NULL;                               // speeds for interpolation 
+    int pose_size = 0;                          // lets initialize our stuff here... 
+    unsigned long next_frame;                   //    
+
+    while(!pbioloid->_fCancel)
+    {
+        // wait for something to do...
+        pthread_mutex_lock(&pbioloid->mutex_);
+        while (!pbioloid->signal_new_pose_) {
+            pthread_cond_wait( &pbioloid->cond_, &pbioloid->mutex_ );
+        }
+        
+        /* Under protection of the lock, complete or remove the work     */
+        /* from whatever worker queue we have. Here it is simply a flag  */
+        // We now copy over the new target positions...
+        if (pbioloid->posesize_ != pose_size ) {
+            pose_size = pbioloid->posesize_ ;
+            if (target_pose)
+                free(target_pose);
+            target_pose = (unsigned int *) malloc(pose_size * sizeof(unsigned int));
+
+            if (servo_speed)
+                free(servo_speed);
+                
+            servo_speed = (int *) malloc(pose_size * sizeof(int));
+        }
+    
+        for (i = 0; i < pose_size; i++)
+            target_pose[i] = pbioloid->nextpose_[i];
+            
+        pbioloid->signal_new_pose_ = 0;
+        
+        uint8_t  frame_length = pbioloid->frameLength;
+
+        pthread_mutex_unlock(&pbioloid->mutex_);
+
+        // We have a new interpolation to handle, so lets set everything up.
+        // Pre calculate each servos fram
+        int frames = (pbioloid->pose_time_ / frame_length) + 1;
+        next_frame = millis() + frame_length;
+        
+        // set speed each servo...
+        int servos_still_moving = 0;
+        for(i=0; i < pose_size; i++){
+            if(target_pose[i] > pbioloid->pose_[i]) {
+                servo_speed[i] = (target_pose[i] - pbioloid->pose_[i])/frames + 1;
+                servos_still_moving++;
+            }
+            else if(target_pose[i] < pbioloid->pose_[i]){
+                servo_speed[i] = (pbioloid->pose_[i]-target_pose[i])/frames + 1;
+                servos_still_moving++;
+            }
+            else
+                servo_speed[i] = 0;
+        }
+        pbioloid->interpolating_ = 1;
+
+//        printf("BWT %d %d\n", frames, servos_still_moving);
+        
+        //  Now lets do our interpolation.  
+        
+        while (!pbioloid->signal_new_pose_ && servos_still_moving && frames--) {
+            // We should delay a frame time now.
+            long delay_time = next_frame - millis();
+            if (delay_time > 0)
+                delay(delay_time);
+
+            next_frame += frame_length;
+
+            // Setup DXL servo packet
+            // update each servo
+            dxl_set_txpacket_id(BROADCAST_ID);
+            dxl_set_txpacket_instruction(AX_SYNC_WRITE);
+            dxl_set_txpacket_parameter(0, AX_GOAL_POSITION_L);
+            dxl_set_txpacket_parameter(1, 2);
+            dxl_set_txpacket_length((2+1)*servos_still_moving+4);
+
+            // Now loop through each of the servos and output those that are still moving.
+            int dxl_parm_index = 2;
+            
+            for(i=0; i < pose_size; i++){
+                if (servo_speed[i]) {
+                    int diff = target_pose[i] - pbioloid->pose_[i];
+                    if(diff > 0){
+                        if(diff < servo_speed[i]){
+                            pbioloid->pose_[i] = target_pose[i];
+                            servo_speed[i] = 0;
+                            servos_still_moving--;
+                        }
+                        else
+                            pbioloid->pose_[i] += servo_speed[i];
+                    } else {
+                        if((-diff) < servo_speed[i]){
+                            pbioloid->pose_[i] = target_pose[i];
+                            servos_still_moving--;
+                            servo_speed[i] = 0;
+                        }
+                        else
+                            pbioloid->pose_[i] -= servo_speed[i];                
+                    }       
+                    
+                    // now add this servo to DXL output.  We might be able to optimize more
+                    diff = (pbioloid->pose_[i]) >> BIOLOID_SHIFT;   // reuse diff... 
+                    dxl_set_txpacket_parameter(dxl_parm_index++, pbioloid->id_[i]);
+                    dxl_set_txpacket_parameter(dxl_parm_index++, dxl_get_lowbyte(diff));
+                    dxl_set_txpacket_parameter(dxl_parm_index++, dxl_get_highbyte(diff));
+                    // if we detected that same position as before...
+                }
+            }
+            // Now lets output the packet. 
+//            printf("dxl_txrx \n");
+            dxl_txrx_packet();
+            dxl_get_result();   // don't care for now
+        }
+        pbioloid->interpolating_ = 0;     // We finished this output...
+    }
+    printf("Bioloid thread exit\n");
+    return 0;
+}
+
+void BioloidControllerEx::end()
+{
+    _fCancel = true;
+    
+   	// pthread_join to sync
+	pthread_join(tidWorker, NULL);
+
+	// destroy barrier
+	pthread_barrier_destroy(&_barrier);
 }
 
 
--- BioloidEX/BioloidEX.h
+++ BioloidEX/BioloidEX.h
@@ -27,6 +27,20 @@
  */
 
 #include <stdint.h>
+#include <pthread.h>
+
+// Added defines to make code easier to read.
+#define AX_ID_DEVICE        0xFD
+#define AX_ID_BROADCAST     0xFE
+
+#define AX_CMD_READ_DATA    0x02
+#define AX_CMD_WRITE_DATA	0x03
+#define AX_CMD_ACTION		0x05
+#define AX_CMD_RESET        0x06
+#define AX_CMD_BOOTLOAD     0x08 
+#define AX_CMD_SYNC_WRITE	0x83
+#define AX_CMD_SYNC_READ    0x84
+
 
 /* pose engine runs at 30Hz (33ms between frames) 
    recommended values for interpolateSetup are of the form X*BIOLOID_FRAME_LENGTH - 1 */
@@ -50,6 +64,7 @@ class BioloidControllerEx
     /* New-style constructor/setup */ 
     BioloidControllerEx() {};
     void setup(int servo_cnt);
+    void end();     
 
     /* Pose Manipulation */
     void loadPose( const unsigned int * addr ); // load a named pose from FLASH  
@@ -68,8 +83,18 @@ class BioloidControllerEx
     void interpolateStep(uint8_t fWait=true);                     // move forward one step in current interpolation  
     uint8_t interpolating(void);                // are we in an interpolation? 0=No, 1=Yes
     unsigned char runningSeq;                   // are we running a sequence? 0=No, 1=Yes 
-    int poseSize;                               // how many servos are in this pose, used by Sync()
+    int poseSize(void);                         // how many servos are in this pose, used by Sync()
+    void poseSize(uint8_t posesize);            // set the pose size
     
+    bool _fCancel;                              // Cancel worker thread.
+    pthread_t tidWorker;                        // Thread Id of our worker thread...
+   	pthread_barrier_t _barrier;
+    pthread_cond_t  cond_; 
+    pthread_mutex_t mutex_;
+    bool signal_new_pose_;                      // The main thread is signaling 
+    int pose_time_;                             // How long is the next pose supposed to take
+    static void *WorkerThreadProc(void *);
+
     // Kurt's Hacks
     uint8_t frameLength;                        // Allow variable frame lengths, to test...
 
@@ -95,17 +120,23 @@ class BioloidControllerEx
      */
     
   private:  
+    uint8_t posesize_;                          // cache the actual pose size.
     unsigned int * pose_;                       // the current pose, updated by Step(), set out by Sync()
     unsigned int * nextpose_;                   // the destination pose, where we put on load
-    int * speed_;                               // speeds for interpolation 
     unsigned char * id_;                        // servo id for this index
 
-//    unsigned long lastframe_;                   // time last frame was sent out  
-    unsigned long nextframe_;                   //    
     transition_t * sequence;                    // sequence we are running
     int transitions;                            // how many transitions we have left to load
     uint8_t interpolating_ ;                    // Actual state.
    
 };
+
+// BUGBUG:: added some AX12 helper functions here. Could/Should create ax12.cpp/h...
+// Some helper functions to map to DXL functionality
+extern int ax12GetRegister(int id, int regstart, int length);
+extern void ax12SetRegister(int id, int regstart, int data);
+extern void ax12SetRegister2(int id, int regstart, int data);
+
+
 #endif
 
